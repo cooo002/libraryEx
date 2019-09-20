8e6c448b02a611dd831823c2255665bc1052846e
cooo002 <cooo002@naver.com>
2019-09-20 11:28:33 +0900

    add .gitignore

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'libraryExercise' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+pod 'Floaty', '~> 4.2.0'
+pod 'JVFloatLabeledTextField'
+
+  # Pods for libraryExercise
+
+  target 'libraryExerciseTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'libraryExerciseUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- libraryExercise/ViewController.swift
+++ libraryExercise/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import Floaty
+import JVFloatLabeledTextField
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        let floaty = Floaty(frame: CGRect(x: self.view.frame.width - 20, y: self.view.frame.height - 20, width: CGFloat(10), height: CGFloat(10)))
+////        floaty.addItem("Hello, World!", icon: UIImage(named: "icon")!)
+////        self.view.addSubview(floaty)
+//        Floaty.global.button.addItem(title: "Hello, World!", handler: { item in
+//            let alert = UIAlertController(title: "확인용 경고창", message: "그저 확인용", preferredStyle: .alert)
+//            alert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+//            self.present(alert, animated: true, completion: nil)
+//
+//        })
+//
+//        Floaty.global.show()
+//    }
+//        var button = JVFloatLabeledTextField(frame: CGRect(x: self.view.frame.width / 2, y: self.view.frame.height / 2, width: 40, height: 20))
+//        
+//    button.layoutSubviews()
+////        self.view.addSubview(button)
+        
+        
+        
+        let floaty = Floaty()
+        floaty.addItem(title: "Hello, World!")
+        self.view.addSubview(floaty)
+        
+        
+        
     }
-
-
 }
 
